707d41e47848b263f721be81bb8dc840750ed223
Tyler Holmes <contact@tylerholmes.com>
2017-12-03 23:21:02 -0800

    Updates message on Contact page (for CI test).

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApp.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page (updated).";
 
             return View();
         }
